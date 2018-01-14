ac678a640a5eb68b748567ae8d4eb88977460c9e
Kelly Boyle <elyobjk@gmail.com>
2018-01-14 18:12:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function sayHiToGrandma(string) {
   if (string === string.toLowerCase()) {
   return `I can\'t hear you!`
 } else if (string === string.toUpperCase()) {
-  return `YES INDEED`
+  return `YES INDEED!`
 } else {return `I love you, too.`
 }
 }
